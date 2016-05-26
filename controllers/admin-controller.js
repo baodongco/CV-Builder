5ca8a363a5b9b165a4e748d44c7a9dc90bbc93ae
Dien Bui <dienbuijoe@outlook.com>
2016-05-26 21:32:07 +0700

    fix bug  + show all users (admin)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,16 @@
+var connection = require('../connection');
+var query = require('../services/user-services');
+
 function adminController() {
     this.getIndex = function (req, res) {
-        if (req.user.role == 'admin')
-            res.render('admin/index', {title: 'Admin page'});
-        else
-            req.flash('homeMessage', 'You do not have priviledge to access.')
+        if (req.isAuthenticated() && req.user.role == 'admin') {
+            connection.pool.query(query.getAllUsers, function (err, users) {
+                res.render('admin/index', {title: 'Admin page', users: users});
+            });             
+        } else {
+            req.flash('homeMessage', 'You do not have priviledge to access.');
             res.redirect('/');
+        }
     };
 }
 
--- services/user-services.js
+++ services/user-services.js
@@ -3,5 +3,6 @@ module.exports = {
     checkUserByUsername: "SELECT id FROM user WHERE username = ?",
     registerUser: "INSERT INTO user SET ?",
     login: "SELECT id, username, password, activationCode, role FROM user WHERE username = ? AND isDisabled = false",
-    getUserById: "SELECT * FROM user WHERE id = ? "
+    getUserById: "SELECT * FROM user WHERE id = ? ",
+    getAllUsers: "SELECT username, email, role, activationCode FROM user"
 };
