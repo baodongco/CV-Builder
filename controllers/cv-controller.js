052668cae2deca55f5439984e43f8821872c0801
Sy Tran Dung <sytrand@HarveyNash.vn.local>
2016-05-27 13:29:07 +0700

    fix user data undefined

---
+++ controllers/cv-controller.js
@@ -0,0 +1,36 @@
+function cvController() {
+    this.getView = function (req, res) {
+        if (req.user)
+            res.redirect('/login');
+        else {     
+            var user = { name: "Tran Dung Sy", email: "sybikhung"};
+            res.render('cv-template/skeleton', {user: user});
+        };                               
+    };
+    this.getDownload = function (req.user, res) {
+        var ejs = require('ejs');
+        var user = { firstName: "Tran", lastName: "Dung Sy", email: "sybikhung"};
+        var file ="";
+        if (req.params.id > 0) {
+            file = "skeleton.ejs";
+        } else {
+            file = "demo.ejs";
+        }
+        ejs.renderFile('./views/cv-template/'+ file, {user: user, style}, null, function (err,html) {
+            if (err) {
+                console.log(err.stack);
+                throw err;
+            } else {
+                var pdf = require('html-pdf');
+                var options = require('cv-pdf.json');
+                options.base = "http://" +req.headers.host;
+                pdf.create(html,options).toStream(function(err, stream){
+                res.setHeader("content-type", "application/pdf");
+                stream.pipe(res);
+            });
+          }
+      });
+    };
+};
+
+module.exports = new cvController();
