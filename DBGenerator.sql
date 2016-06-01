68981d9f119a212d7b94f3b156a1a0b7fab97629
nakedwarrior <sytrancsvn@gmail.com>
2016-06-01 18:37:36 +0700

    add store procedure to get resume

--- DBGenerator.sql
+++ DBGenerator.sql
@@ -229,3 +229,21 @@ BEGIN
 
 END; $$
 DELIMITER ;
+
+DELIMITER $$
+-- Insert user defined store procedure getAllResumeData
+CREATE PROCEDURE udsp_getAllResumeData( IN _resId INT)
+BEGIN   
+  SELECT * FROM resume WHERE id = _resID;
+
+  SELECT * FROM certification where resId = _resID;
+
+  SELECT * FROM education WHERE resId = _resID;
+
+  SELECT * FROM experience WHERE resId = _resID;
+
+  SELECT * FROM project WHERE resId = _resID;
+  
+  SELECT * FROM skill WHERE resId = _resID;
+END; $$
+DELIMITER;
