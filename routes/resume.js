14b87c3d0e0e9e5a0dd3a37c23a81072170cefc2
nakedwarrior <sytrancsvn@gmail.com>
2016-06-03 11:36:56 +0700

    resumes route MVC standard

--- routes/resume.js
+++ routes/resume.js
@@ -3,7 +3,17 @@ var checkLogin = require('../middlewares/check-login')
 
 module.exports = {
 	configure: function (app) {
-		app.post('/createresume', checkLogin, function (req, res) {                          
+    /**
+     * get create resume view
+     */
+    app.get('/resumes/create', function(req,res){
+      resumeController.createResume(req,res);
+    });
+    /**
+     * create new resume
+     * @param  req.body.[data]
+     */
+		app.post('/resumes/create', checkLogin, function (req, res) {                          
 			    resumeController.insertResume(req, res);
 		});
 		/**
@@ -21,33 +31,57 @@ module.exports = {
     app.get('/resumes/:id', function (req, res) {
         resumeController.getResume(req, res);
     });
-
-    app.get('/resumes/:id/preview', function(req, res) {
-        resumeController.getPreview(req, res);
-    }); 
+    /**
+     * get resume preview
+     * @param  id resume
+     */
+    app.get('/resumes/preview/:id', function(req, res) {
+        resumeController.getPreviewResume(req, res);
+    });
+    /**
+     * get publi view of resume
+     * @param  id resume
+     * @param  token
+     */
+    app.get('/resumes/public/:id/:token', function (req, res) {
+        resumeController.getPublicResume(req, res);
+    });
+    /**
+     * get edit resuem view
+     * @param  id resume
+     */
+    app.get('/resumes/edit/:id', function (req, res) {
+        resumeController.getEditResume(req,res);
+    })
 
     /**
-     * change template for a resume
-     * @param  iRd resume Id
-     * @param  tId template Id
+     * update a resume
+     * @param  id of resume
+     * @param  body data
      * @return status code
      */
-    app.get('/resumes/:rId/templates/:tId', function (req, res) {
-      resumeController.updateTemplate(req, res);
+    app.post('/resumes/edit/:id', function (req, res) {
+      resumeController.postEditResume(req, res);
     });
 
     /**
+     * edit a single row value in resume
+     * @param  table : name of table contain resume's data
+     * @param  id: row id to update
+     * @param  field: column to update
+     * @param  value: new value
+     * @return status code
+     */
+    app.post('/resumes/edit-field', function (req, res) {
+      resumeController.postEditFieldResume(req,res);
+    })
+    /**
      * delete a resume
      * @param  id of resume
-     * @return status code 200
+     * @return status code
      */
     app.delete('/resumes/:id', function (req, res) {
       resumeController.deleteResume(req, res);
     })
-
-		app.get('/createresume', function(req,res){
-			resumeController.createResume(req,res);
-		});
-
 	}
 };
