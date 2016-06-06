ba63c81170a659aefe9702dcbe6b077550448c91
lang <bnlinhlang@gmail.com>
2016-06-06 13:01:28 +0700

    User DI for creating resume.

--- DAL/repositories/experience-repository.js
+++ DAL/repositories/experience-repository.js
@@ -2,10 +2,10 @@ var db = require('../connection');
 
 module.exports = function () {
 	this.create = function(data, callback) {
-		db.pool.query('INSERT INTO experience SET', data);
+		db.pool.query('INSERT INTO experience SET ?', data);
 	};
 
 	this.update = function(data, callback) {
-		db.pool.query('UPDATE experience SET', data);
+		db.pool.query('UPDATE experience SET ?', data);
 	};
 };
--- routes/resume.js
+++ routes/resume.js
@@ -1,4 +1,5 @@
 var resumeController = require('../controllers/resume-controller');
+var DIresumeController = require('../controllers/DI-resume-controller');
 var checkLogin = require('../middlewares/check-login')
 
 module.exports = {
@@ -6,50 +7,51 @@ module.exports = {
     /**
      * get create resume view
      */
-    app.get('/resumes/create', checkLogin, function (req, res) {
+     app.get('/resumes/create', checkLogin, function (req, res) {
       resumeController.createResume(req, res);
-    });
+  });
     /**
      * create new resume
      * @param  req.body.[data]
      */
-    app.post('/resumes/create', checkLogin, function (req, res) {
-      resumeController.insertResume(req, res);
-    });
-    app.post('/resumes/photo', function (req, res) {
+     app.post('/resumes/create', checkLogin, function (req, res) {
+      //resumeController.insertResume(req, res);
+      DIresumeController.insertResume(req,res);
+  });
+     app.post('/resumes/photo', function (req, res) {
       resumeController.uploadPhoto(req, res);
-    });
-    app.post('/resumes/photo/load', checkLogin, function (req, res) {
+  });
+     app.post('/resumes/photo/load', checkLogin, function (req, res) {
       resumeController.loadPhoto(req, res);
-    });
+  });
 		/**
       	 * @return resumes list of current user
       	 */
-    app.get('/resumes', checkLogin, function (req, res) {
-      resumeController.getResumes(req, res);
-    });
+         app.get('/resumes', checkLogin, function (req, res) {
+          resumeController.getResumes(req, res);
+      });
     /**
      * get resume preview
      * @param  id resume
      */
-    app.get('/resumes/preview/:id', checkLogin, function (req, res) {
+     app.get('/resumes/preview/:id', checkLogin, function (req, res) {
       resumeController.getPreviewResume(req, res);
-    });
+  });
     /**
      * get publi view of resume
      * @param  id resume
      * @param  token
      */
-    app.get('/resumes/public/:id/:token', function (req, res) {
+     app.get('/resumes/public/:id/:token', function (req, res) {
       resumeController.getPublicResume(req, res);
-    });
+  });
     /**
      * get edit resuem view
      * @param  id resume
      */
-    app.get('/resumes/edit/:id', checkLogin, function (req, res) {
+     app.get('/resumes/edit/:id', checkLogin, function (req, res) {
       resumeController.getEditResume(req, res);
-    })
+  })
 
     /**
      * update a resume
@@ -57,9 +59,9 @@ module.exports = {
      * @param  body data
      * @return status code
      */
-    app.post('/resumes/edit/:id', function (req, res) {
+     app.post('/resumes/edit/:id', function (req, res) {
       resumeController.postEditResume(req, res);
-    });
+  });
 
     /**
      * edit a single row value in resume
@@ -69,29 +71,29 @@ module.exports = {
      * @param  value: new value
      * @return status code
      */
-    app.post('/resumes/edit-field', checkLogin, function (req, res) {
+     app.post('/resumes/edit-field', checkLogin, function (req, res) {
       resumeController.postEditFieldResume(req, res);
-    });
+  });
 
-    app.get('/resumes/privacy/:id', function (req, res) {
+     app.get('/resumes/privacy/:id', function (req, res) {
       resumeController.getPrivacyResume(req, res);
-    });
+  });
     /**
      * @param  id of resume
      * @param  req.type {html|pdf}
      * @return resume file base on type
      */
-    app.get('/resumes/:id', checkLogin, function (req, res) {
+     app.get('/resumes/:id', checkLogin, function (req, res) {
       resumeController.getResume(req, res);
-    });
+  });
 
     /**
      * delete a resume
      * @param  id of resume
      * @return status code
      */
-    app.delete('/resumes/:id', checkLogin, function (req, res) {
+     app.delete('/resumes/:id', checkLogin, function (req, res) {
       resumeController.deleteResume(req, res);
-    })
-  }
+  })
+ }
 };
