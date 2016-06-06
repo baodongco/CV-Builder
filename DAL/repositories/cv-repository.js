15be44c20c37de8aabe72ccaac64519e23498fbf
lang <bnlinhlang@gmail.com>
2016-06-06 10:24:21 +0700

    add title tab

--- DAL/repositories/cv-repository.js
+++ DAL/repositories/cv-repository.js
@@ -1,3 +1,15 @@
-/**
- * Created by dienbui on 6/6/16.
- */
+var db = require('../connection');
+
+module.exports = function () {
+	this.create = function(data, callback) {
+		db.pool.query('INSERT INTO resume SET', data, function(err, rows) {
+			callback(rows);
+		});
+	};
+
+	this.update = function(data, callback) {
+		db.pool.query('UPDATE resume SET', data, function(err, rows) {
+			callback(rows);
+		});
+	};
+};
