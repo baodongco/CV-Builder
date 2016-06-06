c65d6a218b342d93004f9f82245deba2eeb93ce4
Dien Bui <dienbui11@gmail.com>
2016-06-06 19:35:30 +0700

    update admin page

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,15 @@ var adminController = require('../controllers/admin-controller');
 var checkAdmin = require('../middlewares/check-admin-account');
 
 module.exports = {
-    configure: function (app, passport) {
+    configure: function (app) {
         // GET: /register
         app.get('/admin', checkAdmin, function(req, res) {
             adminController.getIndex(req, res);
-        });    
+        });
+
+        app.get('/resumes/:name/:id', checkAdmin, function(req, res) {
+            adminController.listAllCVOfAUser(req, res);
+        });
         
         app.get('/admin/user/disable/:id', function (req, res) {
             adminController.disableUser(req, res);
